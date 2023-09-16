54078a00bf75503ee3e50e7bf62b6a1aeafe905c
robertako97 <roberto.diazgmx@gmail.com>
2023-09-16 12:24:01 -0700

    Need to clean files and add style to post/add submit inputs/buttons on blog home

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('api/users/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -17,8 +17,10 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       // If successful, redirect the browser to the profile page
       document.location.replace('/');
+
+      return response;
     } else {
-      alert(response.statusText);
+      alert('Login failed. Please try again.');
     }
   }
 };
@@ -40,7 +42,8 @@ const signupFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert(response.statusText);
+      alert('Sign Up failed, verify credentials and try again.');
+
     }
   }
 };
